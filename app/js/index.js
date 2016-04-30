da02f80cffef96b850175650790d7ba447407d15
blacksonic <soos.gabor86@gmail.com>
2016-04-30 17:23:37 +0200

    bundled index file

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,8 @@
 
+require('./app.js');
+require('./phone/phone-list.ctrl.js');
+require('./phone/phone.ctrl.js');
+require('./carrier-select.directive.js');
+require('./search-box.directive.js');
+require('./copyright.directive.js');
+require('./short-description.filter.js');
