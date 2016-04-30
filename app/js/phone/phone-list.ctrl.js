547a928baffefc8a7b098ebd2c8b0c3483bfdbc6
blacksonic <soos.gabor86@gmail.com>
2016-04-30 15:33:13 +0200

    watch for filter attributes

--- app/js/phone/phone-list.ctrl.js
+++ app/js/phone/phone-list.ctrl.js
@@ -2,18 +2,31 @@
 
 angular
   .module('phoneApp')
-  .controller('PhoneListController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+  .controller('PhoneListController', ['$scope', '$http', '$rootScope', '$filter', function($scope, $http, $rootScope, $filter) {
     $scope.searchText = '';
     $scope.selectedCarrier = '';
+    $scope.allPhones = [];
     $scope.phones = [];
 
     $rootScope.$on('carrier-selected', function($event, carrier) {
       $scope.selectedCarrier = carrier;
     });
 
+    function getFilteredPhones() {
+      console.log('run')
+      return $filter('filter')($scope.allPhones, { name: $scope.searchText, carrier: $scope.selectedCarrier });
+    }
+
+    $scope.$watch('searchText', function() {
+      $scope.phones = getFilteredPhones();
+    });
+    $scope.$watch('selectedCarrier', function() {
+      $scope.phones = getFilteredPhones();
+    });
+
     $http.get('/api/phones.json')
       .then(function (response) {
-        $scope.phones = response.data;
+        $scope.phones = $scope.allPhones = response.data;
       });
   }]);
 
