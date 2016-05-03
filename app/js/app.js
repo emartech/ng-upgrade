351bd4030c8bb1fd388167dac5bb5acc065fe627
Drawain <drawain@gmail.com>
2016-05-04 00:04:19 +0200

    feat(step2)

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,19 @@
 import { PhoneRepositoryService } from './services/phone-repository/phone-repository.service';
 import { CarrierRepositoryService } from './services/carrier-repository/carrier-repository.service';
 
+import { CopyrightComponent } from './components/copyright/copyright.component';
+
 angular
   .module('phoneApp', [
     'ngRoute'
   ])
 
+  .constant('ACTUAL_YEAR', 2016)
+
   .service('phoneRepository', PhoneRepositoryService.create())
   .service('carrierRepository', CarrierRepositoryService.create())
+
+  .component('copyright', CopyrightComponent.create())
 
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
