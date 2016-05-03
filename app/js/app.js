06191af93a0d394df8e2cb065318220c62ae8a0d
Drawain <drawain@gmail.com>
2016-05-04 00:18:15 +0200

    feat(step2): remove old copyright lib

--- app/js/app.js
+++ app/js/app.js
@@ -37,5 +37,4 @@ require('./phone-list.ctrl.js');
 require('./phone.ctrl.js');
 require('./carrier-select.directive.js');
 require('./search-box.directive.js');
-require('./copyright.directive.js');
 require('./short-description.filter.js');
